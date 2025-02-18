1c31326f444c366bd71a17da169eea4f78331220
Bell Moore <bellmoor@iu.edu>
2025-02-18 11:20:29 -0500

    external JS

--- week6/js/external.js
+++ week6/js/external.js
@@ -1,1 +1,19 @@
 console.log("Ummm... where are we?");
+
+const birthYear = 1207;
+let myName = "Ty the Noodle Arm Guy";
+
+console.log(myName, "was born", birthYear);
+
+myName = "Ty the Fly Guy";
+
+// birthYear = 1208;
+// ^ const variables cannot change value
+
+console.warn(myName, "MAY hve devoured ten souls");
+
+console.log("My name is", myName.length, "characters long");
+
+console.log("My name includes ty", myName.toUpperCase().includes("TY"));
+
+console.log("My name has uppercase", myName !== myName.toLowerCase());
